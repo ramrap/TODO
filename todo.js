ebd9aac3cf4429749c4da6119ea149405af789ea
Rahulparmar01 <rahul220.iit@gmail.com>
2020-04-01 02:49:29 +0530

    added remove function and add function using Jquery

--- todo.js
+++ todo.js
@@ -14,55 +14,81 @@
 // }
 
 
-function addItem(){
+// function addItem(){
 
-    var input=document.getElementById('input');
-    var item=input.value;
-    console.log("value of item is"+item);
+//     var input=document.getElementById('input');
+//     var item=input.value;
+//     console.log("value of item is"+item);
     
 
-    var ul = document.getElementById('list');
-    var textnode = document.createTextNode(item)
-    if(item===''){
-        const pValue=document.getElementById("checkString");
-        pValue.textContent="Enter a TODO";
-    }
-    else{
+//     var ul = document.getElementById('list');
+//     var textnode = document.createTextNode(item)
+//     if(item===''){
+//         const pValue=document.getElementById("checkString");
+//         pValue.textContent="Enter a TODO";
+//     }
+//     else{
         
 
-        //create li
-        var li=document.createElement('li');
-        //create checkbox
-        var checkboc=document.createElement('input');
-        checkboc.type='checkbox';
-        checkboc.setAttribute('id','check');
+//         //create li
+//         var li=document.createElement('li');
+//         //create checkbox
+//         var checkboc=document.createElement('input');
+//         checkboc.type='checkbox';
+//         checkboc.setAttribute('id','check');
 
-        // create label
-        var label =document.createElement('label');
+//         // create label
+//         var label =document.createElement('label');
         
 
         
 
-        //add these elements on web page
-        ul.appendChild(label);
-        li.appendChild(checkboc);
-        label.appendChild(textnode);
-        li.appendChild(label);
-        ul.insertBefore(li, ul.childNodes[0]);
-        //li.className='visual'
-        setTimeout(()=>{
-            li.className='visual';
+//         //add these elements on web page
+//         ul.appendChild(label);
+//         li.appendChild(checkboc);
+//         label.appendChild(textnode);
+//         li.appendChild(label);
+//         ul.insertBefore(li, ul.childNodes[0]);
+//         //li.className='visual'
+//         setTimeout(()=>{
+//             li.className='visual';
 
-        },1
-        );
+//         },1
+//         );
 
-        input.value='';
-
-
-
-
-    }
+//         input.value='';
 
 
 
-}
+
+//     }
+
+
+
+// }
+
+$(document).ready(function(){
+    var input = $('#input').val();
+    
+    $('#add').click(function(){
+        var input = $('#input').val();
+        if(input===''){
+            $('#checkString').text("Enter valid Todo");
+        }
+        else{
+            var strin=`<li class='mycheck'> <input type ="checkbox" id ="check"> <label>${input}</label> </li>`
+            $('#list').prepend(strin);
+         }
+    });
+
+    $('#remove').click(function(){
+        var ul = document.getElementById('list');
+        var li = ul.children;
+         for (var i=0; i < li.length; i++) {
+            while(li[i] && li[i].children[0].checked) {
+                ul.removeChild(li[i]);
+            }
+        }
+    })
+})
+
